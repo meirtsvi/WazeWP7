1cabd9de3a9ff2995522706eab1a7356bc3e851b
Meir Tsvi <meir.tsvi@live.com>
2011-08-11 23:35:23 +0300

    Adjusted makefiles to properly compile

--- bb_waze_code/j2me/run.sh
+++ bb_waze_code/j2me/run.sh
@@ -1,8 +1,8 @@
-if [ -z "${CIBYL_BASE+xxx}" ]; then export CIBYL_BASE=$PWD/../../../cibyl/; export PATH=$PATH:$PWD/../../../cibyl/mips-cibyl-elf/bin/; fi
+if [ -z "${CIBYL_BASE+xxx}" ]; then export CIBYL_BASE=$PWD/../../cibyl/; export PATH=$PATH:$PWD/../../cibyl/mips-cibyl-elf/bin/; fi
 
 cibyl-generate-c-header -o include/ syscalls/ gps_manager sound_mgr timer_mgr json_lib tile_storage async_net waze rim_timer_mgr rimapi_os5 rimapi  ansi java javax.microedition rim_device softfloat
 cd include
-mv cibyl-syscalls.db ../../../../cibyl/mips-cibyl-elf/mips-cibyl-elf/sys-root/usr/include/cibyl-syscalls.db
+mv cibyl-syscalls.db ../../../cibyl/mips-cibyl-elf/mips-cibyl-elf/sys-root/usr/include/cibyl-syscalls.db
 cd ../
 make RIM=YES RIMOS5=YES all
 cd tmpclasses/
