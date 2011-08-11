77c6272a56fa58897eec822d279389bf4bb68dc9
Meir Tsvi <meir.tsvi@live.com>
2011-08-11 23:43:18 +0300

    Dummy commit

--- WazeWP7/RecordStore.cs
+++ WazeWP7/RecordStore.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Xml.Serialization;
 using System.IO.IsolatedStorage;
 
+
     [XmlRoot("RecordStore", Namespace = "RecordStore", IsNullable = false)]
     public class RecordStore
     {
