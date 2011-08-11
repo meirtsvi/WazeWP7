f9f1d2ae69e1602b164f7b238f25737363d10b97
Meir Tsvi <meir.tsvi@live.com>
2011-08-12 00:36:54 +0300

    dummy checking

--- WazeWP7/RecordStore.cs
+++ WazeWP7/RecordStore.cs
@@ -6,6 +6,8 @@ using System.IO;
 using System.Xml.Serialization;
 using System.IO.IsolatedStorage;
 
+
+
     [XmlRoot("RecordStore", Namespace = "RecordStore", IsNullable = false)]
     public class RecordStore
     {
