9459b0417511edf1e1ac53ef15e521ee9c258933
meirtsvi <meir.tsvi@live.com>
2011-10-09 16:29:09 +0200

    Fixed CR/LF for unix

--- bb_waze_code/j2me/run.sh
+++ bb_waze_code/j2me/run.sh
@@ -1,5 +1,5 @@
 if [ -z "${CIBYL_BASE+xxx}" ]; then export CIBYL_BASE=$PWD/../../cibyl/; export PATH=$PATH:$PWD/../../cibyl/mips-cibyl-elf/bin/; fi
-
+mkdir tmpclasses
 cibyl-generate-c-header -o include/ syscalls/ gps_manager sound_mgr timer_mgr json_lib tile_storage async_net waze rim_timer_mgr rimapi_os5 rimapi  ansi java javax.microedition rim_device softfloat
 cd include
 mv cibyl-syscalls.db ../../../cibyl/mips-cibyl-elf/mips-cibyl-elf/sys-root/usr/include/cibyl-syscalls.db
@@ -10,7 +10,3 @@ cibyl-mips2java  --only-translate -d ../tmpclasses ../c/program
 cp *.j ../../../converted
 cp program.data.bin ../../../WazeWP7/Resources/
 cp CibylCallTable.java ../../../WazeWP7/CibylCallTable.cs
-
-
-
-
